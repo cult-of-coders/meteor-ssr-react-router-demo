24dc02c2727e2110a8c5c10509a6f3e7514a56de
Theodor Diaconu <diaconu.theodor@gmail.com>
2017-11-13 16:01:03 +0200

    extracted app outside

--- server/ssr.jsx
+++ server/ssr.jsx
@@ -4,16 +4,14 @@ import { onPageLoad } from "meteor/server-render";
 import { StaticRouter } from 'react-router';
 import {Routes} from '../imports/ui/router';
 
+const App = props => {
+    return (
+        <StaticRouter location={props.location.path} context={{}}>
+            <Routes/>
+        </StaticRouter>
+    )
+};
+
 onPageLoad((sink) => {
-    let context = {};
-
-    const App = props => {
-        return (
-            <StaticRouter location={props.location.path} context={context}>
-                <Routes/>
-            </StaticRouter>
-        )
-    };
-
     sink.renderIntoElementById('app', renderToString(<App location={sink.request.url} />));
 });
